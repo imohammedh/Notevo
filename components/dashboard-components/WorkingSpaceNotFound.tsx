f7e3975bbb5b991f97754bf843d423309aa353db
imohammedhamed <mohammedhamed6726@gmail.com>
2025-02-03 18:23:23 +0200

    change the icon and the font style

--- components/dashboard-components/WorkingSpaceNotFound.tsx
+++ components/dashboard-components/WorkingSpaceNotFound.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Table,Plus } from "lucide-react"
+import { Notebook,Plus } from "lucide-react"
 import { Button } from "../ui/button";
 import MaxWContainer from "../ui/MaxWContainer";
 import { useMutation } from "convex/react";
@@ -19,7 +19,7 @@ export default function WorkingSpaceNotFound() {
         <MaxWContainer>
             <div className="w-full text-center py-14">
                 <div className="flex flex-col items-center justify-center gap-3">
-                <Table size="50" />
+                <Notebook size="50" />
                 <h1 className=" text-3xl font-bold text-center">{`You don't have any working Space `}</h1>
                 </div>
                 <div className="w-full flex items-center justify-center mt-5">
