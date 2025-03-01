27db3c6d1d830214814378e3996a149c75af36cc
imohammedhamed <mohammedhamed6726@gmail.com>
2025-03-01 08:54:38 +0200

    change the widht and the absolute right-2 of the skeleton loading in tables page

--- app/dashboard/[slug]/loading.tsx
+++ app/dashboard/[slug]/loading.tsx
@@ -14,9 +14,9 @@ export default function loading() {
             {Array.from({ length: 3 }).map((_, index) => (
               <div key={index}>
                 <div className="relative group p-3.5 w-full h-40 border border-solid border-brand_tertiary/10 rounded-lg transition-all duration-300">
-                  <div className="skeleton w-28 h-6 bg-brand_tertiary/20 rounded mb-2"></div>
+                  <div className="skeleton w-32 h-6 bg-brand_tertiary/20 rounded mb-2"></div>
                   <div className="skeleton w-28 h-4 bg-brand_tertiary/20 rounded mb-1"></div>
-                  <div className="skeleton w-10 h-8 absolute top-3 right-0 bg-brand_tertiary/20 rounded"></div>
+                  <div className="skeleton w-10 h-8 absolute top-3 right-2 bg-brand_tertiary/20 rounded"></div>
                   <div className="flex justify-center items-center gap-1 absolute bottom-5 left-5">
                     <div className="skeleton w-10 h-4 bg-brand_tertiary/20 rounded"></div>
                   </div>
