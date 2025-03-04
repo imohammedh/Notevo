a1b36e0fdcbe00ee08593491d40b0acbd4bfdba1
imohammedhamed <mohammedhamed6726@gmail.com>
2025-03-04 14:20:20 +0200

    update the FloatingNavbar Style

--- components/dashboard-components/FloatingNavbar.tsx
+++ components/dashboard-components/FloatingNavbar.tsx
@@ -72,94 +72,116 @@ export default function FloatingNavbar() {
   return (
     <div className="fixed w-full bottom-0 right-0 h-16 bg-brand_fourthary/70 backdrop-blur border border-solid border-brand_tertiary/5 block sm:hidden ">
       <div className=" w-full h-full flex justify-center items-center gap-5">
-        <Button
-          variant="Trigger"
-          className="justify-center text-brand_tertiary/50 hover:text-brand_tertiary"
-        >
-          <Link href="/dashboard">
-            <Home size="24" />
-          </Link>
-        </Button>
-        <SearchDialog Variant="Trigger" WithTheTitle={false} IconSize="24" />
-        <Button
-          variant="Trigger"
-          className="justify-center text-brand_tertiary/50 hover:text-brand_tertiary"
-          onClick={
-            searchParams === null ? handleCreateWorkingSpace : handleCreateTable
-          }
-          disabled={handleCreateTableloading}
-        >
-          <Plus size="24" />
-        </Button>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            {Signoutloading === false ? (
-              <Button
-                variant="Trigger"
-                className=" justify-center text-brand_tertiary/50 hover:text-brand_tertiary"
-              >
-                <Avatar className="max-w-9 max-h-9 flex items-center justify-center">
-                  <AvatarImage
-                    src={User?.image}
-                    className=" rounded-lg"
-                    alt={User ? User.name?.charAt(0) : ""}
-                  />
-                  <AvatarFallback>
-                    {User ? User.name?.charAt(0) : <LoadingAnimation />}
-                  </AvatarFallback>
-                </Avatar>
-              </Button>
-            ) : (
-              <Button
-                variant="Trigger"
-                className=" justify-center text-brand_tertiary/50 hover:text-brand_tertiary"
-                disabled={Signoutloading}
-              >
-                <LoadingAnimation />
-              </Button>
-            )}
-          </DropdownMenuTrigger>
-          <DropdownMenuContent
-            side="top"
-            className=" rounded-lg m-2 p-2 bg-brand_fourthary backdrop-blur border border-solid border-brand_tertiary/10 w-fit"
+        <div className=" flex flex-col justify-center items-center text-brand_tertiary/70 text-xs hover:text-brand_tertiary">
+          <Button
+            variant="Trigger"
+            className="justify-center text-brand_tertiary/50 hover:text-brand_tertiary"
           >
-            <DropdownMenuItem className=" w-full">
-              <Button
-                variant="SidebarMenuButton"
-                className=" w-full"
-                disabled={true}
-              >
-                <CircleUserRound size="16" /> Account
-              </Button>
-            </DropdownMenuItem>
-            <DropdownMenuItem className=" w-full">
-              <Button
-                variant="SidebarMenuButton"
-                className=" w-full"
-                disabled={true}
-              >
-                <CreditCard size="16" /> Billing
-              </Button>
-            </DropdownMenuItem>
-            <SidebarSeparator className=" opacity-30 my-2" />
-            <DropdownMenuItem className=" w-full">
-              <Button
-                variant="SidebarMenuButton"
-                className=" w-full"
-                disabled={Signoutloading}
-                onClick={handleSignOut}
-              >
-                {Signoutloading ? (
-                  "Signing out..."
-                ) : (
-                  <>
-                    <LogOut size="16" /> Sign out
-                  </>
-                )}
-              </Button>
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+            <Link href="/dashboard">
+              <Home size="24" />
+            </Link>
+          </Button>
+          Home
+        </div>
+        <div className=" flex flex-col justify-center items-center text-brand_tertiary/70 text-xs hover:text-brand_tertiary">
+          <Button
+            variant="Trigger"
+            className="justify-center text-brand_tertiary/50 hover:text-brand_tertiary"
+          >
+            <SearchDialog
+              Variant="Trigger"
+              WithTheTitle={false}
+              IconSize="24"
+            />
+          </Button>
+          Search
+        </div>
+        <div className=" flex flex-col justify-center items-center text-brand_tertiary/70 text-xs hover:text-brand_tertiary">
+          <Button
+            variant="Trigger"
+            className="justify-center text-brand_tertiary/50 hover:text-brand_tertiary"
+            onClick={
+              searchParams === null
+                ? handleCreateWorkingSpace
+                : handleCreateTable
+            }
+            disabled={handleCreateTableloading}
+          >
+            <Plus size="24" />
+          </Button>
+          Create
+        </div>
+        <div className=" absolute -top-3 right-5 text-brand_tertiary/70 text-xs hover:text-brand_tertiary">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              {Signoutloading === false ? (
+                <Button
+                  variant="Trigger"
+                  className=" justify-center text-brand_tertiary/50 hover:text-brand_tertiary"
+                >
+                  <Avatar className="max-w-14 max-h-14 flex items-center justify-center">
+                    <AvatarImage
+                      src={User?.image}
+                      className=" rounded-lg"
+                      alt={User ? User.name?.charAt(0) : ""}
+                    />
+                    <AvatarFallback>
+                      {User ? User.name?.charAt(0) : <LoadingAnimation />}
+                    </AvatarFallback>
+                  </Avatar>
+                </Button>
+              ) : (
+                <Button
+                  variant="Trigger"
+                  className=" justify-center text-brand_tertiary/50 hover:text-brand_tertiary"
+                  disabled={Signoutloading}
+                >
+                  <LoadingAnimation />
+                </Button>
+              )}
+            </DropdownMenuTrigger>
+            <DropdownMenuContent
+              side="top"
+              className=" rounded-lg m-2 p-2 bg-brand_fourthary backdrop-blur border border-solid border-brand_tertiary/10 w-fit"
+            >
+              <DropdownMenuItem className=" w-full">
+                <Button
+                  variant="SidebarMenuButton"
+                  className=" w-full"
+                  disabled={true}
+                >
+                  <CircleUserRound size="16" /> Account
+                </Button>
+              </DropdownMenuItem>
+              <DropdownMenuItem className=" w-full">
+                <Button
+                  variant="SidebarMenuButton"
+                  className=" w-full"
+                  disabled={true}
+                >
+                  <CreditCard size="16" /> Billing
+                </Button>
+              </DropdownMenuItem>
+              <SidebarSeparator className=" opacity-30 my-2" />
+              <DropdownMenuItem className=" w-full">
+                <Button
+                  variant="SidebarMenuButton"
+                  className=" w-full"
+                  disabled={Signoutloading}
+                  onClick={handleSignOut}
+                >
+                  {Signoutloading ? (
+                    "Signing out..."
+                  ) : (
+                    <>
+                      <LogOut size="16" /> Sign out
+                    </>
+                  )}
+                </Button>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
       </div>
     </div>
   );
