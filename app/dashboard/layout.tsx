f1fb51a3cdc30a27cd3c08ea4ea3477834099ef3
imohammedhamed <mohammedhamed6726@gmail.com>
2025-03-19 05:46:34 +0200

    re add the Breadcrumb and we stel have the production hydration error

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { ReactNode } from "react";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import AppSidebar from "@/components/dashboard-components/AppSidebar";
@@ -10,6 +11,7 @@ export default function DashboardLayout({ children }: { children: ReactNode }) {
         <div className=" w-full fixed top-0 bg-brand_primary/70 backdrop-blur-md [-webkit-backdrop-filter:blur(8px)] [backdrop-filter:blur(8px)] z-50 p-2">
           <div className="flex items-center justify-start gap-2">
             <SidebarTrigger className=" lg:block hidden" />
+            <BreadcrumbWithCustomSeparator />
           </div>
         </div>
         <div className=" mt-16">{children}</div>
