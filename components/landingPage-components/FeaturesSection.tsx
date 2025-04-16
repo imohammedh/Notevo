2e9a26a1d78a798bbc80bc9ea8d3a7ffa01002ff
imohammedhamed <mohammedhamed6726@gmail.com>
2025-04-16 14:25:28 +0200

    delete the comments

--- components/landingPage-components/FeaturesSection.tsx
+++ components/landingPage-components/FeaturesSection.tsx
@@ -119,38 +119,3 @@ export default function FeaturesSection() {
     </Section>
   );
 }
-
-// <WobbleCard containerClassName="col-span-1 lg:col-span-3 bg-gradient-to-br from-brand_fourthary from-15% to-brand_tertiary/20 min-h-[500px] lg:min-h-[600px] xl:min-h-[300px]">
-//   <div className="max-w-sm">
-//     <h2 className="max-w-sm md:max-w-lg  text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-brand_tertiary">
-//       Organize your thoughts
-//     </h2>
-//     <p className="mt-4 max-w-[26rem] text-left  text-base/6 text-neutral-200">
-//       manage your workspaces, and boost your productivity with Notevo.
-//     </p>
-//   </div>
-//   <Image
-//     src={Organize}
-//     width={500}
-//     height={500}
-//     alt="linear demo image"
-//     className="absolute right-10 md:-right-[40%] lg:-right-[10%] -bottom-10 object-contain rounded-2xl"
-//   />
-// </WobbleCard>
-// <WobbleCard containerClassName="col-span-1 lg:col-span-3 bg-gradient-to-br from-brand_fourthary from-15% to-brand_tertiary/20 min-h-[500px] lg:min-h-[600px] xl:min-h-[300px]">
-// <div className="max-w-sm">
-//   <h2 className="max-w-sm md:max-w-lg  text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-brand_tertiary">
-//     Track Daily Activities
-//   </h2>
-//   <p className="mt-4 max-w-[26rem] text-left  text-base/6 text-neutral-200">
-//     manage your workspaces, and boost your productivity with Notevo.
-//   </p>
-// </div>
-// <Image
-//   src={Activities}
-//   width={500}
-//   height={500}
-//   alt="linear demo image"
-//   className="absolute right-10 md:-right-[40%] lg:-right-[10%] -bottom-10 object-contain rounded-2xl"
-// />
-// </WobbleCard>
