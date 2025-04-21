8949d80b78bd17a6b2f13051216a5865e6c8d459
imohammedhamed <mohammedhamed6726@gmail.com>
2025-04-22 00:12:18 +0200

    adjust for mobile view experience

--- components/dashboard-components/AppSidebar.tsx
+++ components/dashboard-components/AppSidebar.tsx
@@ -26,6 +26,7 @@ import {
   SidebarMenuItem,
   SidebarSeparator,
   SidebarTrigger,
+  useSidebar,
 } from "@/components/ui/sidebar";
 import {
   DropdownMenu,
@@ -51,6 +52,8 @@ import { redirect, usePathname } from "next/navigation";
 import { Badge } from "@/components/ui/badge";
 
 export default function AppSidebar() {
+  const { open, isMobile } = useSidebar();
+
   const pathname = usePathname();
   const createWorkingSpace = useMutation(
     api.mutations.workingSpaces.createWorkingSpace,
@@ -103,7 +106,9 @@ export default function AppSidebar() {
               BETA
             </Badge>
           </div>
-          <SidebarTrigger className="transition-all duration-200 ease-in-out  opacity-0 group-hover:opacity-100" />
+          <SidebarTrigger
+            className={`transition-all duration-200 ease-in-out  opacity-0 group-hover:opacity-100 ${(!open || isMobile) && `opacity-100`}`}
+          />
         </div>
       </SidebarHeader>
       <SidebarContent className="bg-brand_fourthary text-brand_tertiary/90 transition-all duration-200 ease-in-out scrollbar-thin scrollbar-thumb-brand_fourthary scrollbar-track-brand_fourthary group-hover:scrollbar-thumb-brand_tertiary">
@@ -126,7 +131,12 @@ export default function AppSidebar() {
                 </Button>
               </SidebarMenuItem>
               <SidebarMenuItem>
-                <SearchDialog showTitle={true} iconSize={16} />
+                <SearchDialog
+                  showTitle={true}
+                  iconSize={16}
+                  sidbarMobile={isMobile}
+                  sidebaraOpen={open}
+                />
               </SidebarMenuItem>
             </SidebarMenu>
           </SidebarGroupContent>
