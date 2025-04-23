6d516dbb7d6275f7c2ed2572fb0f99a7f7add4b5
imohammedhamed <mohammedhamed6726@gmail.com>
2025-04-23 13:04:02 +0200

    change the gradient in the dash hom

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -18,7 +18,7 @@ function DashboardContent({ children }: { children: ReactNode }) {
   }, []);
 
   return (
-    <div className="flex h-screen w-full  bg-gradient-to-r from-brand_fourthary from-15% via-purple-950 to-brand_fourthary overflow-hidden">
+    <div className="flex h-screen w-full  bg-gradient-to-r from-brand_fourthary from-15% via-brand_fourthary to-purple-900 overflow-hidden">
       {isClient && <AppSidebar />}
       <main
         className={`relative flex flex-col flex-1 min-h-svh transition-all duration-300 ease-in-out border-brand_tertiary/20 ${open && !isMobile && `rounded-t-xl border-t border-l mt-3`} rounded-none bg-brand_primary/80`}
