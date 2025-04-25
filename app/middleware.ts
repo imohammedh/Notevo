37ccfae565270a4528d6573e93fb3ebe0f8f0b45
imohammedhamed <mohammedhamed6726@gmail.com>
2025-04-25 17:36:57 +0300

    add the middleware back and put it in the app folder

---
+++ app/middleware.ts
@@ -0,0 +1,23 @@
+import {
+  convexAuthNextjsMiddleware,
+  createRouteMatcher,
+  nextjsMiddlewareRedirect,
+} from "@convex-dev/auth/nextjs/server";
+ 
+const isPublicRoute = createRouteMatcher(["/signin", "/"]); // Include root route
+const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
+ 
+export default convexAuthNextjsMiddleware(async (request, { convexAuth }) => {
+  if (isPublicRoute(request) && (await convexAuth.isAuthenticated())) {
+    return nextjsMiddlewareRedirect(request, "/dashboard");
+  }
+  if (isProtectedRoute(request) && !(await convexAuth.isAuthenticated())) {
+    return nextjsMiddlewareRedirect(request, "/signin");
+  }
+});
+ 
+export const config = {
+  // The following matcher runs middleware on all routes
+  // except static assets.
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
