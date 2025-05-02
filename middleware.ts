068ced0fad699721237fb5534a092c0ebcdca781
imohammedhamed <mohammedhamed6726@gmail.com>
2025-05-03 00:35:00 +0300

    add Configure cookie expiration to the middleware

--- middleware.ts
+++ middleware.ts
@@ -7,17 +7,18 @@ import {
 const isSignInPage = createRouteMatcher(["/signin","/"]);
 const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
  
-export default convexAuthNextjsMiddleware(async (request, { convexAuth }) => {
-  if (isSignInPage(request) && (await convexAuth.isAuthenticated())) {
-    return nextjsMiddlewareRedirect(request, "/dashboard");
-  }
-  if (isProtectedRoute(request) && !(await convexAuth.isAuthenticated())) {
-    return nextjsMiddlewareRedirect(request, "/signin");
-  }
-});
+export default convexAuthNextjsMiddleware(
+  async (request, { convexAuth }) => {
+    if (isSignInPage(request) && (await convexAuth.isAuthenticated())) {
+      return nextjsMiddlewareRedirect(request, "/dashboard");
+    }
+    if (isProtectedRoute(request) && !(await convexAuth.isAuthenticated())) {
+      return nextjsMiddlewareRedirect(request, "/signin");
+    }
+  },
+  { cookieConfig: { maxAge: 60 * 60 * 24 * 30 } }, // Set cookie to expire after 30 days
+);
  
 export const config = {
-  // The following matcher runs middleware on all routes
-  // except static assets.
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
