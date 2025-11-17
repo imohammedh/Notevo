027c0a78947c2c3c8bf766a1312ac8dbd82ecd94
unknown <mohammedhamed6726@gmail.com>
2025-11-17 19:55:27 +0200

    add prefetch to true for the terms and privacy policy

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -178,8 +178,14 @@ export default function SignInPage() {
           </Card>
           <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 hover:[&_a]:text-foreground">
             By signing up and continue, you agree to our{" "}
-            <Link href="/terms-of-service">Terms of Service</Link> and{" "}
-            <Link href="/privacy-policy">Privacy Policy</Link>.
+            <Link prefetch={true} href="/terms-of-service">
+              Terms of Service
+            </Link>{" "}
+            and{" "}
+            <Link prefetch={true} href="/privacy-policy">
+              Privacy Policy
+            </Link>
+            .
           </div>
         </div>
       </div>
