fdcc28f7c5b4a8beccbdf99b54d907e75e157e84
unknown <mohammedhamed6726@gmail.com>
2026-01-17 16:51:04 +0200

    UI/ spacing improvements , Mobile responsiveness enhancements

--- components/home-components/HomeClientLayout.tsx
+++ components/home-components/HomeClientLayout.tsx
@@ -76,7 +76,7 @@ const HomeContent = memo(({ children }: { children: ReactNode }) => {
         </div>
         <div
           ref={scrollContainerRef}
-          className="pt-7 flex-1 overflow-y-auto scrollbar-thin scrollbar-thumb-border scrollbar-track-transparent"
+          className="flex-1 overflow-y-auto scrollbar-thin scrollbar-thumb-border scrollbar-track-transparent"
         >
           {children}
         </div>
