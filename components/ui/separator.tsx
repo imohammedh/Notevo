30b162961b1f49d8e4626225e1a210ce2be6a823
unknown <mohammedhamed6726@gmail.com>
2026-02-19 01:30:36 +0200

    increase the opacity of the separator primitive

--- components/ui/separator.tsx
+++ components/ui/separator.tsx
@@ -18,7 +18,7 @@ const Separator = React.forwardRef<
       decorative={decorative}
       orientation={orientation}
       className={cn(
-        "shrink-0 bg-primary/10 text-primary/10 self-center",
+        "shrink-0 bg-primary/20 text-primary/20 self-center",
         orientation === "horizontal" ? "h-[1px] w-full" : "h-5 mx-1 w-[1px]",
         className,
       )}
